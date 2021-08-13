28ef9203acccb6593731825d1203f1ab5fb18ec6
haakon8855 <haakon8855@gmail.com>
2021-08-13 21:08:15 +0200

    Boids done
    Drawing using PyQt5

---
+++ boids.py
@@ -0,0 +1,97 @@
+"""haakon8855"""
+import numpy as np
+
+
+class Boids():
+    def __init__(self, amount, dim) -> None:
+        self.positions = np.random.rand(amount, 2) * dim
+        angles = np.random.rand(amount) * np.pi * 2
+        self.velocities = np.array([np.cos(angles), np.sin(angles)]).T
+        self.speed = 10
+        self.tooCloseDist = 30
+        self.viewDist = 120
+        self.edgeAvoidance = 0.12
+        self.avoidance = 0.1
+        self.coherence = 0.3
+        self.conformity = 0.1
+
+    def getPositions(self):
+        return self.positions
+
+    def getFilteredBoids(self, index: int):
+        position = self.positions[index]
+        np.delete(self.positions, index, axis=0)
+        np.delete(self.velocities, index, axis=0)
+        filterArray = np.linalg.norm(self.positions - position,
+                                     axis=1) < self.viewDist
+        return self.positions[filterArray], self.velocities[filterArray]
+
+    def getAvoidVector(self, index: int, visiblePositions: np.array):
+        position = self.positions[index]
+        avoidVector = np.zeros(2)
+        tooCloseBoids = visiblePositions[np.linalg.norm(
+            visiblePositions - position, axis=1) < self.tooCloseDist]
+        for pos in tooCloseBoids:
+            diff = pos - position
+            avoidVector = avoidVector - diff
+        return avoidVector
+
+    def getPercievedVelocityVector(self, index: int,
+                                   visibleVelocities: np.array):
+        percievedVelocity = np.zeros(2)
+        for velocity in visibleVelocities:
+            percievedVelocity = percievedVelocity + velocity
+        percievedVelocity = percievedVelocity / len(visibleVelocities)
+        return (percievedVelocity - self.velocities[index]) * self.conformity
+
+    def getPercievedCenter(self, index: int, visiblePositions: np.array):
+        position = self.positions[index]
+        massCenter = visiblePositions.sum(axis=0) / len(visiblePositions)
+        return massCenter - position
+
+    def getEdgeCorrectionVector(self, index: int, maxValues: "list[int]"):
+        position = self.positions[index]
+        edgeCorrectionVelocity = np.zeros(2)
+        offset = maxValues * 0.2
+        if position[0] < offset[0]:
+            edgeCorrectionVelocity[0] = offset[0] - position[0]
+        elif position[0] > maxValues[0] - offset[0]:
+            edgeCorrectionVelocity[0] = maxValues[0] - offset[0] - position[0]
+        if position[1] < offset[1]:
+            edgeCorrectionVelocity[1] = offset[1] - position[1]
+        elif position[1] > maxValues[1] - offset[1]:
+            edgeCorrectionVelocity[1] = maxValues[1] - offset[1] - position[1]
+        return edgeCorrectionVelocity
+
+    @staticmethod
+    def normalize(vector, scale: float):
+        if vector.any():
+            vector = (vector / np.linalg.norm(vector)) * scale
+        return vector
+
+    def move(self, maxValues: "list[int]"):
+        maxValues = np.array(maxValues)
+        newVelocities = self.velocities.copy()
+        for i in range(len(self.positions)):
+            visiblePositions, visibleVelocities = self.getFilteredBoids(i)
+            edgeCorrectionVector = self.getEdgeCorrectionVector(i, maxValues)
+            edgeCorrectionVector = Boids.normalize(edgeCorrectionVector,
+                                                   self.edgeAvoidance)
+            avoidVector = self.getAvoidVector(i, visiblePositions)
+            avoidVector = Boids.normalize(avoidVector, self.avoidance)
+            percievedVelocity = self.getPercievedVelocityVector(
+                i, visibleVelocities)
+            centerVector = self.getPercievedCenter(i, visiblePositions)
+            centerVector = Boids.normalize(centerVector, self.coherence)
+
+            newVelocity = self.velocities[
+                i] + avoidVector + percievedVelocity + edgeCorrectionVector
+            newVelocity = newVelocity / np.linalg.norm(newVelocity)
+            newVelocities[i] = newVelocity
+        newPositions = (self.positions + newVelocities * self.speed)
+        self.positions = newPositions
+        self.velocities = newVelocities
+        return self.positions
+
+    def __str__(self) -> str:
+        return f"pos: {self.position}, dir: {self.direction}"
---
+++ window.py
@@ -0,0 +1,83 @@
+from boids import Boids
+from PyQt5 import QtWidgets, QtGui, QtCore
+import numpy as np
+import sys
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self, fps=60, amount=100) -> None:
+        super().__init__()
+        title = "Boids"
+        top = 200
+        left = 100
+        self.dim = [1400, 900]
+        self.boidSize = 10
+        self.fps = fps
+        self.amount = amount
+
+        self.colors = {
+            "bg": QtGui.QColor(30, 30, 30),
+            "boids": QtGui.QColor(100, 100, 170)
+        }
+
+        self.setWindowTitle(title)
+        self.setGeometry(top, left, self.dim[0], self.dim[1])
+        self.image = QtGui.QImage(self.size(), QtGui.QImage.Format_RGB32)
+        self.image.fill(self.colors["bg"])
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.iteration)
+        self.boids = Boids(amount, self.dim)
+
+    def start(self) -> None:
+        self.timer.start(int(1000 / self.fps))
+
+    def stop(self) -> None:
+        self.timer.stop()
+
+    def iteration(self) -> None:
+        self.boids.move(self.dim)
+        self.drawBoids()
+
+    def paintEvent(self, event) -> None:
+        canvasPainter = QtGui.QPainter(self)
+        canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+
+    def drawBoids(self) -> None:
+        self.image.fill(self.colors["bg"])
+        painter = QtGui.QPainter(self.image)
+
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setPen(QtGui.QPen(self.colors["boids"], 1))
+        painter.setBrush(self.colors["boids"])
+
+        for i, position in enumerate(self.boids.positions):
+            angle = np.arccos(self.boids.velocities[i][0])
+            if (self.boids.velocities[i][1] < 0):
+                angle = -angle
+            angleA = angle
+            angleB = angle - np.pi * (3 / 4)
+            angleC = angle + np.pi * (3 / 4)
+            A = np.array([np.cos(angleA), np.sin(angleA)])
+            A = Boids.normalize(A, 10) + position
+            B = np.array([np.cos(angleB), np.sin(angleB)])
+            B = Boids.normalize(B, 6) + position
+            C = np.array([np.cos(angleC), np.sin(angleC)])
+            C = Boids.normalize(C, 6) + position
+            triangle = QtGui.QPolygon([
+                QtCore.QPoint(A[0], A[1]),
+                QtCore.QPoint(B[0], B[1]),
+                QtCore.QPoint(C[0], C[1])
+            ])
+            painter.drawPolygon(triangle)
+
+        self.update()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.start()
+    window.show()
+
+    sys.exit(app.exec())
