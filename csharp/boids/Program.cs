3cf2920bc651e56d61e5ceab592cfbb5e4d02364
Håkon Anders <haakon8855@users.noreply.github.com>
2021-12-05 21:35:30 +0100

    Added template MonoGame project

---
+++ csharp/boids/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace boids
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var boidsDrawer = new BoidsDrawer())
+                boidsDrawer.Run();
+        }
+    }
+}
